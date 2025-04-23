5e5bfa337a8914609cb5f09ca632243824e55128
Nick Maslov <kategorychnost@gmail.com>
2025-04-23 03:07:20 -0400

    Set up the app structure with navigation and screens

---
+++ styles/auth.styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 50,
+    color: "blue",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+});
